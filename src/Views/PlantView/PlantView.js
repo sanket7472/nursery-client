9f95672c827caad29da0f9b4aebeadb51b27c4f6
sanket7472 <sankettamnar2003@gmail.com>
2024-07-26 08:28:28 +0530

    homepage dane

---
+++ src/Views/PlantView/PlantView.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PlantView() {
+  return (
+    <div>PlantView</div>
+  )
+}
+
+export default PlantView
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter , RouterProvider } from 'react-router-dom';
+import Home from './Views/Home/Home';
+import PlantView from './Views/PlantView/PlantView';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+
+
+
+
+
+const router = createBrowserRouter([
+ { path: '/',
+  element: <Home/>,
+ }
+,
+
+ {
+  path: '/plantview',
+  element: <PlantView/>,
+ },
+
+ {
+  path: '*',
+  element:<h1> 404 NOT FOUND </h1>,
+ }
+])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+<RouterProvider router={router} />);
