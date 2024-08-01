3bc643e214ff316b1991ebf37e8998538552640e
sanket7472 <sankettamnar2003@gmail.com>
2024-08-01 13:13:28 +0530

    update request done

--- src/Components/PlantCard.js
+++ src/Components/PlantCard.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import './PlantCard.css'
-import toast ,{ Toaster}  from 'react-hot-toast';
+import toast, { Toaster } from 'react-hot-toast';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 
 
-function PlantCard({_id, plantname, category, image, price, description, loadPlants}) {
-  
-  const deletePlant = async (plantId) =>{
+function PlantCard({ _id, plantname, category, image, price, description, loadPlants }) {
+
+  const deletePlant = async (plantId) => {
     const response = await axios.delete(`${process.env.REACT_APP_API_URL}/plant/${plantId}`)
 
     toast.success(response.data.message)
@@ -17,20 +17,20 @@ function PlantCard({_id, plantname, category, image, price, description, loadPla
   return (
     <>
       <div className='PlantCard'>
-      <div className='upper-line'> <h1 className='plantname'>{ plantname}</h1>
-      <img src={image} className='plant-image' alt='plant-img'/></div>
-      <div className='category'> Category - {category}</div>
-   
-      <div className='description'> Description - {description}</div>
-      <div className='price'> Price - {price}</div>
+        <div className='upper-line'> <h1 className='plantname'>{plantname}</h1>
+          <img src={image} className='plant-image' alt='plant-img' /></div>
+        <div className='category'> Category - {category}</div>
 
-      <div>
+        <div className='description'> Description - {description}</div>
+        <div className='price'> Price - {price}</div>
+
+        <div className='btn-div'>
           <Link className='plant-card-action-btn' to={`/update/${_id}`}>Edit</Link>
-          <button type='button' className='plant-card-action-btn' 
-            onClick={()=>{
+          <button type='button' className='plant-card-action-btn'
+            onClick={() => {
               deletePlant(_id)
-              }}>
-                Delete
+            }}>
+            Delete
           </button>
         </div>
       </div>
